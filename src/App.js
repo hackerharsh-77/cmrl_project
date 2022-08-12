2e0db817a77f1cae6f84110898b69e6850ca62c7
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2022-08-12 16:14:16 +0530

    vl

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Home from './components/Home/Home'
-import Footer from './components/Footer/Footer'
-import Page2 from './components/Page2/Page2'
-import Station from './components/Station/Station'
-import Feedback from './components/Feedback/Feedback'
-import "./App.css"
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Page2 from "./components/Page2/Page2";
+import Station from "./components/Station/Station";
+import Feedback from "./components/Feedback/Feedback";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import axios from 'axios'
+import axios from "axios";
 // axios.defaults.baseURL = "http://localhost:7777/app/";
 
 const App = () => {
-    return (
-        <div >
-            <Navbar/>
-            {/* <Home/> */}
-            {/* <Page2 /> */}
-            {/* <Station /> */}
-            {<Feedback />}
-            {<Home/>}
-            <Footer />
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="page2" element={<Page2 />} />
+          <Route path="station" element={<Station />} />
+          <Route path="feedback" element={<Feedback />} />
+        </Routes>
+      </BrowserRouter>
 
-export default App
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
