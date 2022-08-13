b5e653f609bc566ff7ca84e184be28a53cd6ca5e
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2022-08-14 00:32:10 +0530

    Updates

---
+++ precache-manifest.911117dda064aa509ac80cf5697b4509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb3f5969afa4413cd90e",
+    "url": "/cmrl_project/static/css/main.ca903b61.chunk.css"
+  },
+  {
+    "revision": "eb3f5969afa4413cd90e",
+    "url": "/cmrl_project/static/js/main.eb3f5969.chunk.js"
+  },
+  {
+    "revision": "cc1608aaa151681c202e",
+    "url": "/cmrl_project/static/js/1.cc1608aa.chunk.js"
+  },
+  {
+    "revision": "a914b3f23e7569b20d60",
+    "url": "/cmrl_project/static/css/2.6340ca02.chunk.css"
+  },
+  {
+    "revision": "a914b3f23e7569b20d60",
+    "url": "/cmrl_project/static/js/2.a914b3f2.chunk.js"
+  },
+  {
+    "revision": "d13111109b7a13808417",
+    "url": "/cmrl_project/static/js/runtime~main.d1311110.js"
+  },
+  {
+    "revision": "1a1b1cf670b871902328ec278ce33a6e",
+    "url": "/cmrl_project/index.html"
+  }
+];
